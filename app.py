bae46dc981419de837e59f65e74fde486bf2cee0
Tanmay Patil <patilts19.comp@coep.ac.in>
2021-11-06 16:12:51 +0530

    Added everything

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, session
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha.fields import RecaptchaField
+from werkzeug.exceptions import RequestTimeout
+from werkzeug.utils import redirect
+from wtforms.fields.core import DateField, StringField
+from wtforms.fields.simple import SubmitField
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '4a7139823b8f9f3f4d5512f7b0b1b9add4cc843c7b4506cba7189305dfe55158'
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name')
+    middle_name = StringField('Middle Name')
+    last_name = StringField('Last Name')
+    # -, /, .
+    dob = StringField('Date of Birth',  render_kw={"placeholder": "dd/mm/yyyy"})
+    mobile = StringField('Mobile')
+    email = StringField('Email')
+    age = StringField('Age')
+    gender = StringField('Gender', render_kw={"placeholder": "Male/Female/Other"})
+    city = StringField('City')
+    state = StringField('State')
+    pin = StringField('Zip Code')
+    aadhar = StringField('Aadhar Number')
+    blood_grp = StringField('Blood Group', render_kw={
+                            'placeholder': 'If O positive then write O+'})
+    dose_num = StringField('Dose Number', render_kw={
+                           "placeholder": "1/2"})
+    prev_id = StringField('Prev Dose ID', render_kw={
+                          "placeholder": "if dose 2 then add prev_id"})
+    prev_date = StringField('Prev Dose Date', render_kw={
+                            "placeholder": "if dose 2 then add prev_date"})
+    vaccine_name = StringField('Vaccine Name', render_kw={
+        'placeholder': 'Covaxin/Covishield/Sputnik'})
+    preferred_date = StringField('Preffered date of Vaccination', render_kw={
+                                 "placeholder": "dd/mm/yyyy"})
+    preferred_time = StringField('Preffered time of Vaccination', default='24 hr format')
+    submit = SubmitField('Register')
+
+@app.route('/')
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        record = dict()
+        print('POSTING')
+        record['first_name'] = form.first_name.data 
+        record['middle_name'] = form.middle_name.data
+        record['last_name'] = form.last_name.data
+        record['dob'] = form.dob.data
+        record['mobile'] = form.mobile.data
+        record['email'] = form.email.data
+        record['age'] = form.age.data
+        record['gender'] = form.gender.data
+        record['city'] = form.city.data
+        record['state'] = form.state.data
+        record['pin'] = form.pin.data
+        record['aadhar'] = form.aadhar.data
+        record['blood_grp'] = form.blood_grp.data
+        record['dose_num'] = form.dose_num.data
+        record['prev_id'] = form.prev_id.data
+        record['prev_date'] = form.prev_date.data
+        record['vaccine_name'] = form.vaccine_name.data
+        record['preferred_date'] = form.preferred_date.data
+        record['preferred_time'] = form.preferred_time.data
+        file = open('records.txt', 'a')
+        file.write(str(record))
+        file.write('\n')
+        file.close()
+        return redirect('register')
+    else:
+        return render_template('register.html', title='Registration From', form=form)
+    return render_template('register.html', title='Registration From', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
